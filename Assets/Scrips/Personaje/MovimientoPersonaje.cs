073e593bc5be8ca778d18cf95fd5ebe9d8062962
David Ezequiel Escalada <metalcombi17@gmail.com>
2024-04-12 01:56:51 -0300

    movPj+linterna
    Se completo el movimiento del jugador
    Rotación del jugador con el movimiento del mouse
    Linterna agregada al jugador y rota con el mouse

--- Assets/Scrips/Personaje/MovimientoPersonaje.cs
+++ Assets/Scrips/Personaje/MovimientoPersonaje.cs
@@ -6,6 +6,13 @@ public class MovimientoPersonaje : MonoBehaviour
 {
     public CharacterController characterController;
     public float velocidad = 15f;
+    public float gravedad = -10;
+    public Transform enElPiso;
+    public float distanciaDelPiso;
+    public LayerMask mascaraPiso;
+
+    Vector3 velocidadAbajo;
+    bool estaEnElPiso;
     void Start()
     {
         
@@ -14,11 +21,20 @@ public class MovimientoPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        estaEnElPiso = Physics.CheckSphere(enElPiso.position, distanciaDelPiso, mascaraPiso);
+        if (estaEnElPiso && velocidadAbajo.y < 0)
+        {
+            velocidadAbajo.y = -2;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 mover = transform.right * x + transform.forward * z;
-
         characterController.Move(mover * velocidad * Time.deltaTime);
+
+        velocidadAbajo.y += gravedad * Time.deltaTime;
+
+        characterController.Move(velocidadAbajo * velocidad * Time.deltaTime);
     }
 }
