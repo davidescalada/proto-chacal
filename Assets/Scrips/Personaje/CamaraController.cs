9ad739e3db00d7dec1f7890d5a1780cb7244d6f2
David Ezequiel Escalada <metalcombi17@gmail.com>
2024-04-11 22:30:45 -0300

    Setup
    base

---
+++ Assets/Scrips/Personaje/CamaraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    public float velocidad = 100f;
+    float rotacionX = 0f;
+    public Transform jugador;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * velocidad * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * velocidad * Time.deltaTime;
+
+        rotacionX -= mouseY;
+        rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotacionX, 0f, 0f);
+        jugador.Rotate(Vector3.up * mouseX);
+    }
+}
