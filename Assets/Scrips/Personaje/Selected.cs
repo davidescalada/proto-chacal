f299347afa3bd7caa0a6efc09b2e7421f73de20d
David Ezequiel Escalada <metalcombi17@gmail.com>
2024-04-12 22:37:08 -0300

    interactuar pt1
    scrip para obj interactuable
    scrip prueba de obj para eliminarse.
    raycast

---
+++ Assets/Scrips/Personaje/Selected.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDeselect : MonoBehaviour
+{
+    LayerMask mask;
+    public float distancia = 1.5f;
+
+    public GameObject textDetect;
+    GameObject ultimoObj = null;
+    Color colorOriginal = Color.white;
+    void Start()
+    {
+        mask = LayerMask.GetMask("Raycast detect");
+        textDetect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Raycast(origen, direccion, out hit, distancia, mascara)
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distancia, mask))
+        {
+            Deselect();
+            SelectObject(hit.transform);
+            if (hit.collider.tag == "Objeto interactivo")
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    hit.collider.transform.GetComponent<Carta>().Eliminar();
+                }
+            }
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distancia, Color.red);
+        }
+        else
+        {
+            Deselect();
+        }
+    }
+
+    void SelectObject(Transform transform)
+    {
+        colorOriginal = transform.GetComponent<MeshRenderer>().material.color;
+        transform.GetComponent<MeshRenderer>().material.color = Color.green;
+        ultimoObj = transform.gameObject;
+    }
+
+    void Deselect()
+    {
+        if (ultimoObj)
+        {
+            ultimoObj.GetComponent<Renderer>().material.color = colorOriginal;
+            ultimoObj = null;
+        }
+    }
+
+    private void OnGUI() //funcion de unity para acceder a los elementos de la ui
+    {
+        if (ultimoObj)
+        {
+            textDetect.SetActive(true);
+        }
+        else
+        {
+            textDetect.SetActive(false);
+        }
+
+    }
+}
